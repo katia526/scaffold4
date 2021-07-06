17d577a2c7c1125c3ca60111024db692d584a95b
katia verachtert <katia.verachtert.scherpendries18@outlook.com>
2021-07-06 15:42:06 +0200

    Add project files.

---
+++ scaffold4/Controllers/AdministrationController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using scaffold4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scaffold4.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RoleCreateViewModel vm)
+        {
+            if (TryValidateModel(vm))
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = vm.Name
+                };
+
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+            }
+
+            return View(vm);
+        }
+    }
+}
---
+++ scaffold4/Models/RoleCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scaffold4.Models
+{
+    public class RoleCreateViewModel
+    {
+        public string Name { get; set; }
+    }
+}
